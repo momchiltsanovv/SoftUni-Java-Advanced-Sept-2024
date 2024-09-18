596fbd514953cc1bb4bf77e235b2c71ffbfb9b7a
Momchil Tsanov <momchil395@gmail.com>
2024-09-18 22:17:14 +0300

    sets 1 2

---
+++ src/_02_SetsAndMapsAdvanced/SetsOfElements.java
@@ -0,0 +1,44 @@
+package _02_SetsAndMapsAdvanced;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+public class SetsOfElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+
+        int sizeFirstSet = sc.nextInt();
+        int sizeSecondSet = sc.nextInt();
+        Set<Integer> firstSetNumbers = new LinkedHashSet<>();
+        Set<Integer> secondSetNumbers = new LinkedHashSet<>();
+
+
+        for (int i = 0; i < sizeFirstSet ; i++) {
+            firstSetNumbers.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < sizeSecondSet; i++) {
+            secondSetNumbers.add(sc.nextInt());
+        }
+
+        firstSetNumbers.retainAll(secondSetNumbers);
+
+        for(int num : firstSetNumbers){
+            System.out.print(num + " ");
+        }
+        sc.close();
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/_02_SetsAndMapsAdvanced/_01_UniqueUsernames.java
@@ -0,0 +1,31 @@
+package _02_SetsAndMapsAdvanced;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+public class _01_UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+
+
+        int size = sc.nextInt();
+        sc.nextLine();
+
+        Set<String> setOfUsernames = new LinkedHashSet<>();
+
+
+        for (int i = 0; i < size; i++) {
+            String username = sc.nextLine();
+            setOfUsernames.add(username);
+        }
+
+        for(String username : setOfUsernames) {
+            System.out.println(username);
+        }
+
+
+    }
+}
