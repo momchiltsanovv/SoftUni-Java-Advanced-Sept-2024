008f2064dc03a44afc3a19a6b282b819e2c8d13a
Momchil Tsanov <momchil395@gmail.com>
2024-10-15 10:45:47 +0300

    exap prep exercise SpaceCraft 90/100

---
+++ src/_10_ExamPrep/_03_SpaceCraft/spaceCrafts/Main.java
@@ -0,0 +1,9 @@
+package _10_ExamPrep._03_SpaceCraft.spaceCrafts;//might need to remove
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 90/100 some edge case - cant find it
+    }
+}
